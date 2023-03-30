71f6aeb6ae847b3332a9dfb7714a2823ce8840c1
sarahcabral <sarah.ly2009@gmail.com>
2023-03-30 20:40:04 -0300

    Teste cripto desenvolvido

--- src/Aluno.js
+++ src/Aluno.js
@@ -6,7 +6,14 @@ module.exports = class Aluno {
         this.premium = false;
         this.id = uuidv4();
         this.cursosMatriculados = new Array();
+        this.carteira = 0.0;
     }
+
+
+    setPremium(){
+        this.premium = !this.premium;
+    }
+
 
     addCurso(curso) {
         this.cursosMatriculados.push(curso);
@@ -14,5 +21,10 @@ module.exports = class Aluno {
 
     hasPremium(numeroCursosConcluidos) {
 
+    }
+
+    exchangeCripto(numCursosConcluidos){
+        let valor = this.cursosMatriculados.length;
+        this.carteira = this.carteira + (0.0005 * valor);
     }
 }
--- tests/Aluno.test.js
+++ tests/Aluno.test.js
@@ -24,3 +24,32 @@ test('Dado um aluno cadastrado e com 12 cursos concluídos deve liberar premium'
     //Assert
     expect(aluno.premium).toBe(true);
 });
+
+
+test('Dada uma troca pelo premium feita, trocar por cripto', () =>{
+
+    //Arrange
+    let aluno = new Aluno("Sarah");
+    aluno.setPremium();
+    aluno.addCurso(new Curso("Curso 1","Descricao bla", 3));
+    aluno.addCurso(new Curso("Curso 2","Descricao blaa", 4));
+    aluno.addCurso(new Curso("Curso 3","Descricao blaaa", 10));
+    aluno.addCurso(new Curso("Curso 4","Descricao blaaaa", 12));
+    aluno.addCurso(new Curso("Curso 5","Descricao blaaaaa", 1));
+    aluno.addCurso(new Curso("Curso 6","Descricao blaaaaaa", 2));
+    aluno.addCurso(new Curso("Curso 7","Descricao blaaaa", 3));
+    //Tirar os comentários para passar
+
+    // aluno.addCurso(new Curso("Curso 8","Descricao blaaaa", 10));
+    // aluno.addCurso(new Curso("Curso 9","Descricao blaaaa", 20));
+    // aluno.addCurso(new Curso("Curso 10","Descricao blaaaa", 8));
+    // aluno.addCurso(new Curso("Curso 11","Descricao blaaaa", 3));
+    // aluno.addCurso(new Curso("Curso 12","Descricao blaaaa", 3));
+
+
+    //Act
+    aluno.exchangeCripto(aluno.cursosMatriculados.length);
+
+    //Assert
+    expect(aluno.carteira).toBeGreaterThanOrEqual(0);
+});
